1743d0e755bfc7f618894864ba71d83137a52568
Stefan Weber <StefanM.Weber@yahoo.com>
2015-07-19 20:02:07 +0200

    implemented standard GNG, Reservoir Computing and some testcases

---
+++ GNG/GNG.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python3
+import numpy as np
+import scipy as sp
+import scipy.spatial.distance
+
+
+
+NaN = float('NaN')
+
+class GNG:
+    
+    def __init__(self):
+        self.NumOfEpochs = 600;
+        self.NumOfSamples = 400;
+        self.age_inc = 1;
+        self.max_age = 200;
+        self.max_nodes = 160;
+        self.eb = .05;
+        self.en = .005;
+        self.Nlambda = 350;
+        self.alpha = .5; 
+        self.d = .99;
+        self.RMSE = np.zeros(self.NumOfEpochs);
+        self.distance_function = 'euclidean'
+        self.nodes = None
+
+    def fit(self,X):
+        np.random.shuffle(X) ##randomize input sequence
+        Ncounter = 0
+        #step 0: initialization
+        #start with two inputs as initial nodes
+        self.nodes = X[-2:,:] ## initial nodes
+        #make a initial Edge between the nodes
+        self.edges = np.array([[0,1],[1,0]])
+        #initialize age matrix
+        self.ages = np.array([[NaN, 0], [0,NaN]])
+        #initialize error vector
+        self.error = np.array([0,0])
+ 
+        for kk in range(self.NumOfEpochs):
+            # Choose the next Input Training Vectors.
+            nextblock = np.random.choice(X.shape[0],self.NumOfSamples)
+            In = X[nextblock,:]
+            for n in range(self.NumOfSamples):
+                Ncounter += 1
+                #step2 find nearest neighbors:
+                inp = In[n,:]
+                ds = np.squeeze(scipy.spatial.distance.cdist( inp.reshape((1,inp.shape[0])), self.nodes, self.distance_function))
+                winner, secondbest, *_ = np.argsort(ds)
+                #Steps 3-6. Increment the age of all edges emanating from the winner node
+                self.edgeManagement(inp, ds, (winner,secondbest))
+                #Step 7. Dead Node Removal Procedure.
+                self.removeUnconnected()
+                # Step 8. Node Insertion Procedure.
+                if (Ncounter % self.Nlambda == 0 and self.nodes.shape[0] < self.max_nodes):
+                    self.addNewNeuron()
+                # Step 9. Finally, decrease the error of all units.
+                self.error = self.d*self.error
+
+    def edgeManagement(self, inp, ds, winners):
+        # Step 3. Increment the age of all edges emanating from s1. 
+        fst, snd = winners
+        fstneighbors = self.neighborsOf(fst)
+        self.ages[fstneighbors,fst]+=self.age_inc
+        self.ages[fst,fstneighbors] = self.ages[fstneighbors,fst]
+        #step4. Add the squared distance to a local error counter variable:
+        self.error[fst] += ds[fst]**2
+        # Step 5. Move s1 and its topological neighbors towards î.
+        self.nodes[fst,:] += self.eb*(inp-self.nodes[fst,:])
+        self.nodes[fstneighbors,:] += self.en*(inp-self.nodes[fstneighbors,:])
+        # Step 6.
+        # If s1 and s2 are connected by an edge, set the age of this edge to zero.
+        # If such an edge does not exist, create it.
+        self.edges[fst,snd] = 1
+        self.edges[fst,snd] = 1
+        self.ages[fst,snd] = 0
+        self.ages[fst,snd] = 0
+        #step 7. remove eges older than max_age
+        mask = self.ages > self.max_age
+        self.edges[mask] = 0
+        self.ages[mask] = NaN
+    def removeUnconnected(self):
+        indices = np.squeeze(np.argwhere(np.sum(self.edges,axis=1))) #find columns with one non zero entry
+        mask = np.ix_(indices,indices)
+        self.edges = self.edges[mask]
+        self.ages = self.ages[mask]
+        self.nodes = self.nodes[indices,:]
+        self.error = self.error[indices]
+
+        pass
+
+    def addNewNeuron(self):
+        q =np.argmax(self.error) # neuron with maximal error
+        mEneighbors = self.neighborsOf(q)
+        p = mEneighbors[np.argmax(self.error[mEneighbors])] #neighbor with maximal error of neuron with maximal error
+        self.nodes = np.vstack([self.nodes, self.nodes[(q,p),:].mean(axis=0)]) # insert new neuron between both
+        self.edges[q,p] = 0
+        self.edges[p,q] = 0
+        self.ages[q,p] = NaN
+        self.ages[p,q] = NaN
+        #increase size in edge and age matrix
+        (i,j) = self.edges.shape
+        en = np.zeros((i+1,j+1))
+        en[:i,:j] = self.edges
+        an = np.zeros((i+1,j+1))
+        an[:i,:j]  = self.ages
+        
+        #add initial edges
+        en[p,i] = 1
+        en[q,i] = 1
+        en[i,p] = 1
+        en[i,q] = 1
+        self.edges = en
+        #add initial ages
+
+        an[q,i] = 0
+        an[i,q] = 0
+        an[p,i] = 0
+        an[i,p] = 0
+        self.ages = an
+    
+        self.error[q] *= self.alpha
+        self.error[p] *= self.alpha
+
+        self.error = np.append(self.error,self.error[q])
+
+    def neighborsOf(self,q):
+        return np.squeeze(np.argwhere(self.edges[:,q]), axis=1)
--- GNGReservoir.py
+++ GNGReservoir.py
@@ -1,40 +1,170 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.4
 import numpy as np
 import scipy as sp
 from numpy.linalg import norm
 from sklearn.preprocessing import normalize
+import sklearn.linear_model
 
 sigmaSq = 6
 
-def genInitValues(numInp, numHidd, numOutp):
-    inp = sp.rand(numInp)
-    states = sp.rand(numHidd)
-    outp = sp.rand(numOutp)
-    N = numInp+numHidd+numOutp
-    M = numHidd+numOutp
+
+def genInitValues(numInp, numHidd):
+    N = numInp+numHidd
+    M = numHidd
     density = 1/sp.sqrt(N)
     W = sp.sparse.rand(N, M, density)
     negValDensity = 0.3
     nM = sp.sign(sp.rand(N, M)-1+negValDensity)
     W = W.multiply(nM)
-    W = normalize(W, axis=1, copy=False)
-    return (inp, states, outp, sp.sparse.csr_matrix(W))
+    W = normalize(W, axis=0, copy=False).T
+    return (sp.sparse.csr_matrix(W))
 
-def step(inp, states, outp, W):
-    S = np.concatenate([inp, states, outp])
+
+def oneOfKtoStr(ook):
+    if sp.sparse.issparse(ook):
+        ook = ook.toarray()
+    indices = np.argmax(ook,axis=1)
+    s = ''
+    for i in indices:
+        if i==0:
+            s+='.'
+        elif i==1:
+            s+=','
+        elif i==2:
+            s+=' '
+        elif i==3:
+            s+='\''
+        else: s+=chr(i+ord('a')-4)
+    return(s)
+def oneOfK(string):
+    i = 0
+    row = []
+    col = []
+    data = []
+    d = 0
+    for x in string:
+        if x.islower():
+            d = ord(x)-ord('a')+4
+        elif x.isupper():
+            d = ord(x)-ord('A')+4
+        elif x=='.':
+            d = 0
+        elif x == ',':
+            d = 1
+        elif x == ' ':
+            d = 2
+        elif x == '\'':
+            d = 3
+        else:
+            continue
+        row.append(i)
+        col.append(d)
+        data.append(1.0)
+        i+=1
+    return sp.sparse.csr_matrix((data, (row,col)), shape=(i,30))
+
+def step(inp, states, W):
+    S = np.concatenate([np.squeeze(inp), states])
     Xcurr = generateSparseActivationMatrix(S, W)
-    actNext = sp.exp(-(1/2*sigmaSq)*norm(Xcurr-W,axis=1)**2)
-    print(actNext.shape)
-    (statesNext, outpNext) =np.split(actNext,states.shape)
-    return (np.squeeze(normalize(statesNext)), np.squeeze(normalize(outpNext)))
+    Diff = (Xcurr-W)
+    Diff.data = Diff.data**2
+    Xnorm = np.array(Diff.sum(axis=1))
+    statesNext = np.squeeze(sp.exp(-(1/2*sigmaSq)*Xnorm))
+    return statesNext
+
+def computeOutputs(states,RR):
+    return sp.squeeze(RR.predict(states))
+
+def runWithStimuli(inp, W, RR=None, states=None):
+    if states is None:
+        states = sp.zeros(W.shape[0])
+
+    for i in range(inp.shape[0]):
+        states = step(inp[i,:].toarray().T, states, W)
+        if RR is None:
+            yield states
+        else:
+            out = computeOutputs(states, RR)
+            yield out,states
+
+def trainWeights(inp, outp, W, a):
+    states = runStimulitoArray(inp,W)
+    RR = sklearn.linear_model.Ridge(alpha=a)
+    if sp.sparse.issparse(outp):
+        outp=outp.toarray()
+    RR.fit(states, outp)
+    return RR
+
+def runStimulitoArray(stim, W, RR=None):
+    if RR is None:
+        return sp.array([val for val in runWithStimuli(stim, W)]) 
+    else:
+        x = [val for val in runWithStimuli(stim, W, RR)]
+        out, states = zip(*x)
+        return sp.array(out),sp.array(states)
 
 
 
 def generateSparseActivationMatrix(S, W):
-    return W.multiply(np.tile(S,[W.shape[1],1]).T)
+    Wc = W.copy()
+    Wc.data = S[Wc.indices]
+    Wc = normalize(Wc,axis=1, norm='l2')
+    return Wc
 
+def testcase2(filename, trainsamples, numStates, lastChars, alpha):
+    with open(filename,'r') as f:
+        cont = f.read()
+        inp1 = oneOfK(cont[:trainsamples])
+        *x1, y1 = genlistOfShiftedStrings(inp1)
+        x1 = sp.sparse.hstack(x1).tocsr()
+
+        W = genInitValues(x1.shape[1], numStates)
+        RR = trainWeights(x1, y1, W, alpha)
+        states = sp.zeros(W.shape[0])
+        x = oneOfK('lala').toarray().reshape(1,120)
+        s = ''
+        for i in range(1000):
+            states = step(x,states,W)
+            out = computeOutputs(states,RR)
+            x[0,0:90] = x[0,30:120]
+            x[0,90:120] = out
+            s += oneOfKtoStr([out])
+        return s
+
+genlistOfShiftedStrings = lambda inp: [inp[i:-(lastChars-i)] for i in range(0,lastChars)]
+
+def testcase1(filename, trainsamples, testsamples, numStates, lastChars, alpha):
+    with open(filename,'r') as f:
+        cont = f.read()
+
+        inp1 = oneOfK(cont[:trainsamples])
+        *x1, y1 = genlistOfShiftedStrings(inp1)
+        x1 = sp.sparse.hstack(x1).tocsr()
+
+        inp2 = oneOfK(cont[trainsamples:trainsamples+testsamples])
+        *x2, y2 = genlistOfShiftedStrings(inp2)
+        x2 = sp.sparse.hstack(x2).tocsr()
+        print('finished generation of testsamples')
+        W = genInitValues(x1.shape[1], numStates)
+        print('initialized network randomly')
+        RR = trainWeights(x1, y1, W, alpha)
+        print('finished training')
+        out1, _ = runStimulitoArray(x1, W, RR)
+        out2, _ = runStimulitoArray(x2, W, RR)
+        print('ran on testset')
+        return oneOfKtoStr(y1),oneOfKtoStr(out1), oneOfKtoStr(y2), oneOfKtoStr(out2)
 
 
 
 if __name__ == '__main__':
-    main()
+    filename = 'shakespeare.txt'
+    trainsamples = 15000
+    lastChars = 5
+    numStates = 3000
+    testsamples = 2000
+    alpha=1e-4
+    y1,out1,y2,out2 = testcase1(filename, trainsamples, testsamples, numStates, lastChars, alpha)
+    print('\noriginal traindata:\n%s\nreconstructed traindata:\n%s\noriginal testdata:\n%s\nrecosntructed testdata:\n%s\n' % (y1,out1,y2,out2))
+
+    s = testcase2(filename, trainsamples, numStates, lastChars, alpha)
+    print(s)
